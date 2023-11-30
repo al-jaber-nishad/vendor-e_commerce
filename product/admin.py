11ff062ad1969587aff8d55837b03bb12a3bb4f0
aj_nishad <mailnishad02@gmail.com>
2023-11-30 15:08:14 +0600

    Update product model and admin

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+	list_display = [field.name for field in Brand._meta.fields]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+	list_display = [field.name for field in Category._meta.fields]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+	list_display = [field.name for field in Product._meta.fields]
+
+
+@admin.register(Color)
+class ColorAdmin(admin.ModelAdmin):
+	list_display = [field.name for field in Color._meta.fields]
+
+
+@admin.register(ProductColor)
+class ProductColorAdmin(admin.ModelAdmin):
+	list_display = [field.name for field in ProductColor._meta.fields]
+
+
+@admin.register(Size)
+class SizeAdmin(admin.ModelAdmin):
+	list_display = [field.name for field in Size._meta.fields]
+
+
+@admin.register(ProductSize)
+class ProductSizeAdmin(admin.ModelAdmin):
+	list_display = [field.name for field in ProductSize._meta.fields]
--- product/models.py
+++ product/models.py
@@ -1,3 +1,131 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.fields.related import ForeignKey
+
+from authentication.models import Vendor
 
 # Create your models here.
+
+class Brand(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='brand/', null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.SET_NULL, related_name="+", null=True, blank=True)
+    updated_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.SET_NULL, related_name="+", null=True, blank=True)
+
+    class Meta:
+        ordering = ('-id', )
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    icon = models.FileField(upload_to="category/", blank=True, null=True)
+    image = models.ImageField(upload_to="category/", blank=True, null=True)
+    parent = ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.SET_NULL, related_name="+", null=True, blank=True)
+    updated_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.SET_NULL, related_name="+", null=True, blank=True)
+
+    class Meta:
+        ordering = ['-id',]
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, null=True, blank=True)
+    description = models.TextField(blank=True, null=True)
+    brand = models.ForeignKey(Brand, on_delete= models.SET_NULL, null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    vendor = models.ForeignKey(Vendor, on_delete= models.SET_NULL, null=True, blank=True)
+    unit_price = models.DecimalField(default=0, max_digits=20, decimal_places=2)
+    thumbnail = models.ImageField(upload_to="product/", null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.SET_NULL, related_name="+", null=True, blank=True)
+    updated_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.SET_NULL, related_name="+", null=True, blank=True)
+
+    class Meta:
+        ordering = ('-id',)
+
+    def __str__(self):
+        return f"{self.id}: {self.name}"
+
+
+class Color(models.Model):
+    name = models.CharField(max_length=255)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.SET_NULL, related_name="+", null=True, blank=True)
+    updated_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.SET_NULL, related_name="+", null=True, blank=True)
+
+    class Meta:
+        ordering = ('id', )
+
+    def __str__(self):
+        return self.name
+
+
+class ProductColor(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True)
+    color = models.ManyToManyField(Color, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.SET_NULL, related_name="+", null=True, blank=True)
+    updated_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.SET_NULL, related_name="+", null=True, blank=True)
+
+    class Meta:
+        ordering = ('id',)
+    
+    def __str__(self):
+        return f"{self.product.name}: {self.size.name}"
+
+
+class Size(models.Model):
+    name = models.CharField(max_length=255)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.SET_NULL, related_name="+", null=True, blank=True)
+    updated_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.SET_NULL, related_name="+", null=True, blank=True)
+
+    class Meta:
+        ordering = ('id',)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductSize(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, blank=True)
+    size = models.ManyToManyField(Size, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.SET_NULL, related_name="+", null=True, blank=True)
+    updated_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete= models.SET_NULL, related_name="+", null=True, blank=True)
+
+    class Meta:
+        ordering = ('id',)
+        
+    def __str__(self):
+        return f"{self.product.name}: {self.size.name}"
